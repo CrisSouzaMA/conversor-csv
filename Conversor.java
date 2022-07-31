2609af60b40d4de38e42a8a58503ecc38ff198ca
CrisSouzaMA <cris.souzamoreiraealmeida@gmail.com>
2022-07-31 15:21:19 -0300

    Feat: implementa método que irá formatar cpf para formato xxx.xxx.xxx-xx

--- Conversor.java
+++ Conversor.java
@@ -14,4 +14,9 @@ public class Conversor {
     new Conversor().converterPasta(pastaDeEntradas, pastaDeSaidas);
   }
 
+  public String novoCpf(String cpf) {
+    return cpf.substring(0, 3) + "."
+            + cpf.substring(3, 6) + "."
+            + cpf.substring(6, 9) + "-" + cpf.substring(9, 11);
+  }
 }
